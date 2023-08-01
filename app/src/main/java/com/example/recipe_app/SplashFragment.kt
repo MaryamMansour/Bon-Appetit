7d7626d5cbe648be3cb053208eff8cfce1c62c1e
Mohamed Zarea <mohamedzarea29@gmail.com>
2023-08-01 10:13:27 +0300

    splash

--- app/src/main/java/com/example/recipe_app/SplashFragment.kt
+++ app/src/main/java/com/example/recipe_app/SplashFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 
 class SplashFragment : Fragment() {
@@ -13,5 +20,16 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch (Dispatchers.IO){
+            delay(5000)
+            withContext(Dispatchers.Main){
+                view.findNavController().navigate(R.id.loginFragment)
+            }
+        }
+
     }
 }
